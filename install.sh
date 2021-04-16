784d060387bdde4e9fd31856d1b5b64e35de8b01
Doug Plant <doug@mugo.ca>
2021-04-16 15:52:57 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# run (as root) like: bash install.sh readerbound
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This has to be run as root" 
+   exit 1
+fi
+
+plainuser="$1"
+
+homefolder="/home/$plainuser"
+
+if [ ! -d "homefolder" ] 
+then
+    echo "Home folder does not exist: "$homefolder 
+    exit 1
+fi
+
+yum -y install vim-enhanced
+
+cd "$homefolder"
+git clone https://github.com/dougplant/dfp-dotfiles.git
+chown -R $plainuser:$plainuser dfp-dotfiles/
+
+echo "alias dfp='. $homefolder/dfp-dotfiles/.bashrc_dfp'" >> .bashrc
+echo "alias dfp='. $homefolder/dfp-dotfiles/.bashrc_dfp'" >> /root/.bashrc
