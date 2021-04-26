50340a27e3b4407fb525534776203e8886d9c586
Doug Plant <doug@mugo.ca>
2021-04-25 19:51:51 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,4 +35,5 @@ echo "alias dfp='. $homefolder/dfp-dotfiles/.bashrc_dfp'" >> /root/.bashrc
 
 # create symlink so that the vi/vim aliases point at the single vimrc file
 cd /root
+rm -f dfp-dotfiles
 ln -s /home/readerbound/dfp-dotfiles/
